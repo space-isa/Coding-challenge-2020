ac9607c30dfed4e440248563d7ef1ec09629b261
Isabel J <31943741+space-isa@users.noreply.github.com>
2020-04-06 16:06:22 -0700

    20200406 upload

--- Ordered-words/findOrderedWords.py
+++ Ordered-words/findOrderedWords.py
@@ -5,10 +5,8 @@ import sys
 
 def findOrderedWords(filename):
     '''This function takes a .txt file as an input and prints all the words that are in alphabetical order.'''
-    assert filename.split('.')[1] == 'txt', "Please use a .txt file."
     with open(filename) as data_file:
         data = data_file.read()
-        assert (len(data) == 0) == False, "There's nothing in this file."
         # If a line remains unchanged after sorting, it can be printed.
         for line in data.splitlines():
             if line == ''.join(sorted(line)):
