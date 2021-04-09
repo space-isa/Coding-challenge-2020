7ff24e8d88ad45f8dd684491ca76ca919bf6e99d
Isabel J. Rodriguez <rodrjack@oregonstate.edu>
2021-04-09 07:54:14 -0700

    adding timing decorator and random input function

--- Add-up-to-k/add_up_to_k.py
+++ Add-up-to-k/add_up_to_k.py
@@ -16,7 +16,7 @@ since 10 + 7 is 17.
 
 SOLUTION APPROACH
 ----------------- 
-1. Validate input (assume int and float). 
+1. Validate input (assume positive int and float). 
 2. If the list is not empty, create an empty storage set. 
 3. For each element in the list, take the difference of k and the element and 
    check if that difference exists in the storage set. 
@@ -25,38 +25,78 @@ SOLUTION APPROACH
 
 Notes:
 Other solutions would include: 
-   - Traversing only a subset of the list containing values less than k. This 
-     would involve sorting [O(n log n)], creating a new list [O(len(l1) - len(l2))], 
+   - Traversing only a subset of the list containing values less than k (assuming positive). 
+     This would involve sorting [O(n log n)], creating a new list [O(len(l1) - len(l2))], 
      and traversing that list O(len(l2)).  
    - Checking each element against every other element. 
  
 This approach has a time complexity of O(n), as the worst case the list only 
 has to be traversed once. 
 """
-    
-def find_sums(my_list=None, k=None):
+import time
+from functools import wraps
+import random
+import numpy as np
+
+
+def timeit(function):
+    """A timing decorator."""
+    @wraps(function)
+    def timing_wrapper(*args):
+        print(function.__name__)
+        start_time = time.time()
+        result = function(*args)
+        end_time = time.time()
+        runtime = end_time - start_time
+
+        print("A list with {} elements took {} {:.2e} sec to process.".format(len(args[0]), 
+                                                                function.__name__, runtime))
+        return result
+    return timing_wrapper
+
+def draw_random_inputs():
+    """Return a list of unique random integers and an integer target."""
+    size = np.random.randint(30)
+    high = size + np.random.randint(10)
+    my_list = np.random.choice(np.arange(0, high), replace=False, size=size)
+    print("my_list =", my_list)
+    k = np.random.randint(100)
+    print("k =",k)
+    return my_list, k
+
+@timeit
+def find_sums(my_list, k):
     # Validate input
     try:
-        if (my_list or k == None) == True: 
-            return("Please provide a list of numbers and a target value.")
-        elif len(my_list) == 0: 
-            return("This list is empty.")
-        else: 
+        if len(my_list) == 0: 
+            return ("This list is empty.")
+        else:
+            elements_checked = 0
+            length = len(my_list) 
             stored_values = set()
-            for i in range(len(my_list)):
-                test_value = my_list[i]
-                difference = k - test_value
+            for element in my_list:
+                elements_checked += 1
+                difference = k - element
                 # Check for a match 
                 if difference in stored_values:
+                    print("Target found in {}/{} elements".format(elements_checked, length))
                     return True
-                    break
                 # If no match, add value to set and continue
-                stored_values.add(my_list[i])  
+                else: 
+                    stored_values.add(element)
+            # Target was not found.
+            return ("No match found. Checked {}/{} elements.".format(elements_checked, length))  
     except TypeError:
         raise TypeError("Please use integers or floats.")
     except Exception as error: 
         print(error)
+    
+def main():
+    my_list, k = draw_random_inputs()
+    result = find_sums(my_list, k)
+    print(result)
 
 
 if __name__ == "__main__":
-    find_sums(my_list ,k)
+    main()
+    
